0b4cb9edd9ac4dc23dd90217e9da7ad5ac89b3a6
Junaid8217 <junaidhossain77349@gmail.com>
2025-11-14 01:10:04 +0600

    Added banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BannerImg1 from '../../assets/vector1.png';
+
+const Banner = () => {
+    return (
+        <div className=' max-w-[1200px] mx-auto flex justify-between items-center my-10 gap-4'>
+            <div className='w-[708px] h-[250px] bg-amber-300 flex flex-col justify-center items-center text-center gap-2 text-white'
+                style={{
+                    backgroundImage: `url(${BannerImg1}), linear-gradient(to right, rgba(99,46,227,0.8), rgba(159,98,242,0.8))`
+                }}>
+                <p className='text-3xl'>In-Progress</p>
+                <h1 className='font-semibold text-6xl'>0</h1>
+            </div>
+            <div className='w-[708px] h-[250px] bg-amber-300 flex flex-col justify-center items-center text-center gap-2 text-white'
+                style={{
+                    backgroundImage: `url(${BannerImg1}), linear-gradient(to right, rgba(84, 207, 104, 1), rgba(0, 130, 122, 1))`
+                }}>
+                <p className='text-3xl'>Resolved</p>
+                <h1 className='font-semibold text-6xl'>0</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
